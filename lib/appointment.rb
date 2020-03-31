8da07d55b2d97987d76a715476599868c8596448
Bikram Sodhi <biksodhi@gmail.com>
2020-03-31 15:00:34 +0000

    Automatically backed up by Learn

--- lib/appointment.rb
+++ lib/appointment.rb
@@ -1,6 +1,6 @@
 class Appointment
 
-  attr_reader
+  attr_reader :date, :doctor, :patient
 
   @@all = []
 
@@ -14,3 +14,5 @@ class Appointment
     @doctor = doctor
     @@all << self
   end
+
+end
