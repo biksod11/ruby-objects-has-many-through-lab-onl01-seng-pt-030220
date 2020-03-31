34c9d54697d84f047f74744f402f14e306b91a4d
Bikram Sodhi <biksodhi@gmail.com>
2020-03-31 14:04:27 +0000

    Automatically backed up by Learn

---
+++ lib/artist.rb
@@ -0,0 +1,28 @@
+class Artist
+
+  @@all = []
+
+  def self.all
+    @@all
+  end
+  
+  def initialize(name)
+    @name = name
+    @@all << self
+  end
+
+  def new_song(name, genre)
+    @name = name
+    @genre = genre
+  end
+
+  def songs
+    Song.all.select {|song| song.artist == self}
+  end
+  
+
+
+
+
+
+end
