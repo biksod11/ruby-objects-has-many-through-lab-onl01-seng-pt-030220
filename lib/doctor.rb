f7e1e8b057457db6b639ed133bd3dcd78bf3260c
Bikram Sodhi <biksodhi@gmail.com>
2020-03-31 15:29:36 +0000

    Automatically backed up by Learn

--- lib/doctor.rb
+++ lib/doctor.rb
@@ -3,16 +3,16 @@ class Doctor
 
   @@all = []
 
+  def self.all
+    @@all
+  end
+
   def initialize(name)
     @name = name
     @@all << self
   end
 
-  def self.all
-    @@all
-  end
-
-  def new_appointment(patient, date)
+  def new_appointment(date, patient)
     Appointment.new(date, patient, self)
   end
 
